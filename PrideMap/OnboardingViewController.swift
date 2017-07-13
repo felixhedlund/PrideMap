891fa4fa509fb916222a6b9ca83861cc71426820
Felix Hedlund <felix.hedlund@jayway.com>
2017-07-13 20:14:35 +0200

    Launchscreen

---
+++ PrideMap/OnboardingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  OnboardingViewController.swift
+//  PrideMap
+//
+//  Created by Felix Hedlund on 2017-07-13.
+//  Copyright © 2017 Felix Hedlund. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
